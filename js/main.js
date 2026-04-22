503f75b94482c35795f8e5dfe04f6033297abfea
Dhiman Sutradhar <dhimans7047@gmail.com>
2026-04-22 10:35:14 +0530

    UPDATED

--- js/main.js
+++ js/main.js
@@ -227,3 +227,20 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const birthDate = new Date('2006-01-28');
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  document.querySelector('.age span').textContent = age;
+});
